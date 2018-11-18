66496200c6068dd28ff4ff621a0d6f45e20cca5e
infsolution <clsinfsolution@gmail.com>
2018-11-17 22:07:13 -0300

    refactor action.js

--- visiomake/js/actions.js
+++ visiomake/js/actions.js
@@ -1,19 +1,8 @@
-function ola(){
-	alert("Eu sou javascript");
-}
-
 function audio_desc(element){
     const espeak_links = document.getElementById(element);
     espeak_links.play(); 
 }
 
 function show_content(element, text){
-	//var text_show = document.getElementById(text);
 	var show_item = document.getElementById(element).innerHTML=document.getElementById(text).innerHTML;
-	//show_item.style.display = "block";
-	/*if(show_item.style.display == "block"){
-		show_item.style.display = "none";
-	}else{
-		show_item.style.display = "block"
-	}*/
 }
